8799f4a936e42096dcd6acd5c9aee7b4c8e65fa2
joao <jvcazarini@gmail.com>
2025-05-17 16:07:21 -0300

    criando botao do cardapio

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+var imgAtual = "../assets/hossomaki.jpg";
+var imgAnterior = "../assets/uramaki.jpg";
+
+function trocar(){
+    document.getElementById("rodizio").src =imgAtual;
+    let aux =imgAtual;
+    imgAtual=imgAnterior
+    imgAnterior=aux
+}var imgAtual = "../assets/hossomaki.jpg";
+var imgAnterior = "../assets/uramaki.jpg";
+
+function trocar(){
+    document.getElementById("rodizio").src =imgAtual;
+    let aux =imgAtual;
+    imgAtual=imgAnterior
+    imgAnterior=aux
+}
